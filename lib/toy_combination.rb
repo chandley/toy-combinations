2ab8509ea1247ac9b98a6334bb8a97d688e45401
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-13 21:27:37 +0000

    shows bunny head, bit too large

--- lib/toy_combination.rb
+++ lib/toy_combination.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class ToyCombination < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views") }
+  set :public_folder, Proc.new { File.join(root, "../public") }
+  
   get '/' do
     'Hello ToyCombination!'
+    erb :toy
+
+
   end
 
   # start the server if ruby file executed directly
